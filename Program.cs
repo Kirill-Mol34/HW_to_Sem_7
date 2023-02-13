effd41671439e809866e032c18235b1d3c924678
Kirill <kir.molokoff@yandex.ru>
2023-02-13 22:02:27 +0900

    finCommit

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿//    Написать программу, которая обменивает элементы первой строки и последней строки.
+/*
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+int[,] ReplaceRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int value = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = value;
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int row = 4;
+int col = 4;
+int min = 1;
+int max = 10;
+
+Console.WriteLine("Массив:");
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Массив с поменянными местами элементами:");
+int[,] matrix2 = ReplaceRow(matrix);
+PrintMatrix(matrix2);
+*/
+
+//    Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+/*
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+int[,] SortRowAscending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+            }
+            int tmp = array[i, j];                                          
+            array[i, j] = array[i, minPosition];                            
+            array[i, minPosition] = tmp;                                    
+        }
+    }
+    return array;
+}
+void PrintTable(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+int row = 4;
+int col = 10;
+int min = 10;
+int max = 100;
+
+Console.WriteLine();
+Console.WriteLine("Массив:");
+int[,] table = GetMatrix(row, col, min, max);
+PrintTable(table);
+Console.WriteLine();
+Console.WriteLine("Упорядоченный массив:");
+int[,] rowSort = SortRowAscending(table);
+PrintTable(rowSort);
+*/
+
+//    В прямоугольной матрице найти строку с наименьшей суммой элементов.
+/*
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+int rowNumber(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 6;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine();
+int index = rowNumber(matrix);
+Console.WriteLine($"в строке номер {rowNumber(matrix)} наименьшая сумма элементов");
+Console.WriteLine();
+*/
